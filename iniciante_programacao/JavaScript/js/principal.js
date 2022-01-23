bafb1d4343a633a1b02a6160a03401ea543301de
Izac-Silva <izaclislanio@gmail.com>
2022-01-23 09:21:50 -0300

    JavaScript, selecionando elementos no DOM

--- iniciante_programacao/JavaScript/js/principal.js
+++ iniciante_programacao/JavaScript/js/principal.js
@@ -1,3 +1,34 @@
 var titulo = document.querySelector('.titulo')
-
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var celulaImc = paciente.querySelector(".info-imc");
+
+
+var imc = peso / (altura * altura);
+celulaImc.textContent = imc;
+
+var pesoValido = true;
+var alturaValido = true;
+
+if (peso < 0 || peso > 400) {
+    celulaImc.textContent = "Peso Inválido"
+    var pesoValido = false;
+}
+
+if (altura < 1 || altura > 3.00) {
+    celulaImc.textContent = "altura Inválido"
+    var alturaValido = false;
+}
+
+if (pesoValido && alturaValido) {
+    var imc = peso / (altura * altura);
+    celulaImc.textContent = imc;
+}
