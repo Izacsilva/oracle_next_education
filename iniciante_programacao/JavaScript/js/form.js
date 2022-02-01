7e6204586005deb6a5407355e605513f4799592e
Izac-Silva <izaclislanio@gmail.com>
2022-02-01 10:16:43 -0300

    melhorando com funcoes

--- iniciante_programacao/JavaScript/js/form.js
+++ iniciante_programacao/JavaScript/js/form.js
@@ -5,66 +5,23 @@ botaoAdicionar.addEventListener('click', function(event){
 
     // Seleciono o formulário no DOM
     const formulario = document.querySelector('#form-adiciona')
-    const campoNome = document.querySelector('#nome')
-    const campoPeso = document.querySelector('#peso')
-    const campoAltura = document.querySelector('#altura')
-    const campoGordura = document.querySelector('#gordura')
-    
-    // Capturo todos os valores dos elementos da tabela
 
-    // Através da função obtem Dados retorno
+    /* Com a variável paciente chamo a função obtemDadosPacientes
+    que captura do meu parâmentro que é um form, os dados inserido
+    pelo usuário. */
+
     let paciente = obtemDadosPacientes(formulario);
 
-    // let nome = formulario.nome.value;
-    // let peso = formulario.peso.value;
-    // let altura = formulario.altura.value;
-    // let gordura = formulario.gordura.value;
-
-    // Crio variável com elementos('tag') HTML <tr> e <td> 
-    // let pacienteTr = document.createElement('tr');
-    montaTr('paciente')
-    /* Eu estava selecionando, não criando elementos */
-    // let nomeTd = document.querySelector('td');
-    // let pesoTd = document.querySelector('td');
-    // let alturaTd = document.querySelector('td');
-    // let gorduraTd = document.querySelector('td');
-    // let imcTd = document.querySelector('td');
-
-    /* O correto é createElement, que irá criar um elemento HTML */
-    let nomeTd = document.createElement('td');
-    let pesoTd = document.createElement('td');
-    let alturaTd = document.createElement('td');
-    let gorduraTd = document.createElement('td');
-    let imcTd = document.createElement('td');
-
-    // Agora coloco os valores dentro dos elementos com a propriedade
-    // textContent, colocando o valor para o DOM (jogando no HTML)
-    // 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-
-    // Dentro da tabela do formulário vou adicionar o <tr> com os <td>
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    /* MontaTr é uma função que cria uma tag <tr> no HTML*/
+    let pacienteTr = montaTr(paciente);
 
     let tabela = document.querySelector('#tabela-pacientes')
 
     tabela.appendChild(pacienteTr)
     
-    // Limpo os campos após enviar os dados para o formulário.
-    campoNome.value = ''
-    campoPeso.value = ''
-    campoAltura.value = ''
-    campoGordura.value = ''
+    // Limpar campos após enviar os dados para o formulário.
 
-
+    formulario.reset();
     
 })
 
@@ -80,9 +37,33 @@ function obtemDadosPacientes(formulario) {
     }
 
     return paciente
-} 
-
-function montaTr(classe) {
-    let pacienteTr = document.createElement('tr');
-        pacienteTr.classList.add(classe)
 }
+
+function montaTd(dado, classe) {
+    let td = document.createElement('td');
+        td.textContent = dado;
+        td.classList.add(classe)
+
+        return td
+    // Estava falhando pois a função não tinha retorno
+}
+
+function montaTr(paciente) {
+    let pacienteTr = document.createElement('tr');
+    pacienteTr.classList.add('paciente');
+
+    let nome_tr = montaTd(paciente.nome, 'info-nome');
+    let peso_tr = montaTd(paciente.peso, 'info-peso');
+    let altura_tr = montaTd(paciente.altura, 'info-altura');
+    let gordura_tr = montaTd(paciente.gordura, 'info-gordura');
+    let imc_tr = montaTd(paciente.imc, 'info-imc');
+
+    pacienteTr.appendChild(nome_tr);
+    pacienteTr.appendChild(peso_tr);
+    pacienteTr.appendChild(altura_tr);
+    pacienteTr.appendChild(gordura_tr);
+    pacienteTr.appendChild(imc_tr);
+
+    return pacienteTr
+}
+
