e80cf588a60e0eec7fb6030f58367a9d8869e3f0
Izac-Silva <izaclislanio@gmail.com>
2022-02-15 13:08:11 -0300

    introducao ao JavaScript

---
+++ iniciante_programacao/JavaScript/primeiros_passos/modulo_01-introducao_linguagem/primeiro_programa.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa escrito com JavaScript");
+
+const idade = 34;
+
+console.log('idade')
+console.log(idade)
+console.log(idade + 2)
+console.log(idade * 2)
+console.log(idade / 2)
