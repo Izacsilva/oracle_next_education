89577ebd2f3ccced34f9f38b024ed1a9772a166d
Izac-Silva <izaclislanio@gmail.com>
2022-02-26 08:08:45 -0300

    backup dos arquivos

---
+++ front-end/javascript/interfaces_herancas_oo/backupFiles/ContaPoupanca.js
@@ -0,0 +1,45 @@
+export class ContaPoupanca{
+    constructor(saldoInicial, cliente, agencia) {
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+// Só adicione cliente se for objeto de cliente.
+
+set cliente(novoValor) {
+    // só atribua o valor se o novoValor for uma instância.
+    if(novoValor instanceof Cliente) {
+        this._cliente = novoValor
+    }
+    
+}
+
+
+
+    sacar(valor) {
+
+        if(this._saldo >= valor) {
+
+            this._saldo -= valor
+            return valor
+        } else {
+            console.log(`Saldo insuficiente ${this._saldo}`)
+        }
+    };
+
+    depositar(valor) {
+
+        if(valor > 0) {
+            this._saldo += valor
+        } else {
+            console.log(`Essa operação não pode ser efetuada! O valor ${valor} é incorreto.`)
+        }
+    };
+    
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado)
+    }
+
+}
