6133400ad7fdbbe857c635a0ea4ed83c240b417d
Izac-Silva <izaclislanio@gmail.com>
2022-03-16 08:31:52 -0300

    curso manipulando_DOM, criando elementos no DOM

--- front-end/javascript/manipulando_DOM/script.js
+++ front-end/javascript/manipulando_DOM/script.js
@@ -1,5 +1,44 @@
-const botao = document.querySelector('[data-form-btn]')
+(() => {function criarTarefas(evento) {
+    
+    evento.preventDefault() //evita o reloud da página
 
-botao.addEventListener('click', () => {
-    console.log('fui clicado.')
-})
+    // Pesquisa(busca) elementos no DOM
+    const lista = document.querySelector("[data-list]")
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+    
+    
+    // Cria apartir do JS um elemento <li> no html
+    const tarefa = document.createElement("li")
+    tarefa.classList.add('item-list') // Adiciona um classe a li
+
+    const conteudo = `<p class="content">${valor}</p>` // Variável que cria um <p> e insere o valor do campo.
+    
+    tarefa.innerHTML = conteudo // Colocando o valor da variavel ao Elemento <li>
+    tarefa.appendChild(BotaoConclui())
+    lista.appendChild(tarefa) // e o colocando dentro do pai(<ul>) no DOM
+    
+    
+
+    input.value = " "
+}
+
+const botao = document.querySelector('[data-form-btn]')
+botao.addEventListener('click', criarTarefas)
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+
+    botaoConclui.classList.add('btn-check')
+    botaoConclui.innerText = '✔'
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
+}
+
+function concluirTarefa(evento) {
+    const botaoConclui = evento.target // Pega o alvo. Como tem um evento no botão ele irá executar a função.
+
+    const tarefaCompleta = botaoConclui.parentElement // Busca o Pai do elemento.
+    tarefaCompleta.classList.toggle('done') // Aplica a classe.
+}})()
